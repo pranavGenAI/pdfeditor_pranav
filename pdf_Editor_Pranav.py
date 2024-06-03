1d1330f97b89c2c2bfc0e8f1f53f028100310a84
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-06-04 02:57:56 +0530

    pdf_Editor_Pranav.py

--- pdf_Editor_Pranav.py
+++ pdf_Editor_Pranav.py
@@ -264,9 +264,7 @@ def otherfunctions():
                             file_name=f"{session_state['name'].rsplit('.')[0]}_rotated_{angle}.pdf",
                             use_container_width=True,
                         )
-            with lcol.expander("🤏 Reduce PDF size"):
-                st.markdown("Coming soon...")
-  
+
             with rcol.expander("↔ Resize/Scale PDF"):
                 # TODO: Add password back to converted PDF if original was protected
                 st.caption("Will remove password if present")
@@ -317,9 +315,9 @@ def otherfunctions():
                         )
 
             
-            # with lcol.expander("🤏 Reduce PDF size"):
-            #     st.markdown("Coming soon...")
-            #     # # TODO: Add password back to converted PDF if original was protected
+            with lcol.expander("🤏 Reduce PDF size"):
+                st.markdown("Coming soon...")
+                # # TODO: Add password back to converted PDF if original was protected
                 # st.caption("Will remove password if present")
 
                 # pdf_small = pdf
